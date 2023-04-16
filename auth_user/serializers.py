ab05ab36b2e06551ba138381c87d1e348833b292
Xoldarov Temur <69597351+xaldarof@users.noreply.github.com>
2023-04-16 20:15:30 +0500

    fix

--- auth_user/serializers.py
+++ auth_user/serializers.py
@@ -8,7 +8,6 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create(
             username=validated_data['username'],
-            email=validated_data['email']
         )
         user.set_password(validated_data['password'])
         user.save()
